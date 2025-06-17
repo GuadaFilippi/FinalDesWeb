bae22c00d27ebc22229462ba40b54e048afc6029
guadalupe filippi <filippiguadalupe@gmail.com>
2025-06-17 16:56:56 -0300

    Versión inicial del gestor de tareas monolítico

---
+++ gestorTareas/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+
+let tareas = JSON.parse(fs.readFileSync('db.json'));
+
+app.get('/tareas', (req, res) => res.json(tareas));
+
+app.post('/tareas', (req, res) => {
+  const nueva = { id: Date.now(), ...req.body };
+  tareas.push(nueva);
+  fs.writeFileSync('db.json', JSON.stringify(tareas, null, 2));
+  res.status(201).json(nueva);
+});
+
+app.delete('/tareas/:id', (req, res) => {
+  tareas = tareas.filter(t => t.id != req.params.id);
+  fs.writeFileSync('db.json', JSON.stringify(tareas, null, 2));
+  res.sendStatus(204);
+});
+
+app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
